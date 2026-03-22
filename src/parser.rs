6a920cd98033a0a170986bd057b94e6726f2cfb9
PurpleCosmic <eliasverhelst7@gmail.com>
2026-03-22 13:25:05 +0100

    finish the spoiler parser :D

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,5 @@
 use chumsky::prelude::*;
 
-#[derive(Debug, PartialEq)]
-pub enum Expr<'src> {
-    Slot(&'src str),
-}
-
 #[derive(Debug, PartialEq, Clone)]
 pub struct Slot {
     player: String,
@@ -35,6 +30,16 @@ pub fn slot_parser<'src>() -> impl Parser<'src, &'src str, Slot> {
         .map(|s: &str| Slot {
             player: String::from(s),
         })
+}
+
+pub fn slots_parser<'src>() -> impl Parser<'src, &'src str, Vec<Slot>> {
+    any()
+        .and_is(slot_parser().not())
+        .repeated()
+        .ignore_then(slot_parser())
+        .repeated()
+        .collect::<Vec<_>>()
+        .then_ignore(any().repeated())
 }
 
 fn parse_from_existing_slot<'src>(slots: &Vec<Slot>) -> impl Parser<'src, &'src str, Slot> {
@@ -105,20 +110,42 @@ fn sphere_parser<'src>(slots: &Vec<Slot>) -> impl Parser<'src, &'src str, Sphere
 }
 
 pub fn playthrough_parser<'src>(slots: &Vec<Slot>) -> impl Parser<'src, &'src str, Vec<Sphere>> {
-    just("Playthrough")
-        .padded()
-        .then_ignore(just(":"))
-        .padded()
-        .then_ignore(just("\n").repeated())
+    any()
+        .and_is(just("Playthrough").not())
+        .repeated()
         .ignore_then(
-            (sphere_parser(slots).then_ignore(just("\n").repeated()))
-                .repeated()
-                .collect::<Vec<_>>(),
+            just("Playthrough")
+                .padded()
+                .then_ignore(just(":"))
+                .padded()
+                .then_ignore(just("\n").repeated())
+                .ignore_then(
+                    (sphere_parser(slots).then_ignore(just("\n").repeated()))
+                        .repeated()
+                        .collect::<Vec<_>>(),
+                ),
         )
+        .then_ignore(any().repeated())
+}
+
+pub fn parse_spoiler(input: &String) -> (Vec<Slot>, Vec<Sphere>) {
+    let slots = slots_parser()
+        .parse(&input)
+        .into_result()
+        .expect("Invalid Slots");
+
+    let playthrough = playthrough_parser(&slots)
+        .parse(&input)
+        .into_result()
+        .expect("Invalid Playthrough");
+
+    (slots, playthrough)
 }
 
 #[cfg(test)]
 mod test_parser {
+    use std::fs;
+
     use super::*;
     fn slot_names() -> Vec<Slot> {
         vec![
@@ -150,6 +177,42 @@ mod test_parser {
             Ok(Slot {
                 player: String::from("slot12")
             })
+        );
+    }
+
+    #[test]
+    fn it_parses_a_sequence_of_slot_names() {
+        assert_eq!(
+            slots_parser()
+                .parse("Player 1: slot1\nblebleble\nblo\nPlayer 2: slot2\nPlayer 3: slot13\n")
+                .into_result(),
+            Ok(vec![
+                Slot {
+                    player: String::from("slot1")
+                },
+                Slot {
+                    player: String::from("slot2")
+                },
+                Slot {
+                    player: String::from("slot13")
+                },
+            ])
+        );
+        assert_eq!(
+            slots_parser()
+                .parse("blabla\nPlayer 1: slot1\nblebleble\nblo\nPlayer 2: slot2\nPlayer 3: slot13")
+                .into_result(),
+            Ok(vec![
+                Slot {
+                    player: String::from("slot1")
+                },
+                Slot {
+                    player: String::from("slot2")
+                },
+                Slot {
+                    player: String::from("slot13")
+                },
+            ])
         );
     }
 
@@ -295,7 +358,7 @@ mod test_parser {
     fn it_parses_playthroughs() {
         assert_eq!(
             playthrough_parser(&slot_names())
-                .parse("Playthrough:\n\n\n1: {\n  loc1 (slot1): item1 (slot2)\n  loc2 (slot1): item2 (slot2)\n}")
+                .parse("Playthrough:\n\n\n1: {\n  loc1 (slot1): item1 (slot2)\n  loc2 (slot1): item2 (slot2)\n}\n")
                 .into_result(),
             Ok(vec![vec![
                 SpoilerEntry {
@@ -361,6 +424,77 @@ mod test_parser {
                     },
                 ]
             ])
+        );
+    }
+
+    #[test]
+    fn it_parses_spoilers() {
+        let spoiler_text =
+            fs::read_to_string("./tests/dummy_spoiler.txt").expect("Could not read file");
+
+        assert_eq!(
+            parse_spoiler(&spoiler_text),
+            (
+                vec![
+                    Slot {
+                        player: String::from("slot1")
+                    },
+                    Slot {
+                        player: String::from("slot2")
+                    },
+                    Slot {
+                        player: String::from("hi_I_have_name")
+                    }
+                ],
+                vec![
+                    vec![
+                        SpoilerEntry {
+                            location: String::from("Server"),
+                            sender: String::from("Archipelago"),
+                            item: String::from("Anakin Skywalker (Jedi)"),
+                            receiver: String::from("slot1")
+                        },
+                        SpoilerEntry {
+                            location: String::from("Server"),
+                            sender: String::from("Archipelago"),
+                            item: String::from("Blelt"),
+                            receiver: String::from("slot2")
+                        },
+                    ],
+                    vec![
+                        SpoilerEntry {
+                            location: String::from("Farm Arrays - Echo"),
+                            sender: String::from("slot1"),
+                            item: String::from("Shrek's Swamp Unlock"),
+                            receiver: String::from("slot2")
+                        },
+                        SpoilerEntry {
+                            location: String::from("Beat Meg"),
+                            sender: String::from("hi_I_have_name"),
+                            item: String::from("Meg Victory"),
+                            receiver: String::from("hi_I_have_name")
+                        },
+                        SpoilerEntry {
+                            location: String::from("Clear Score 0001"),
+                            sender: String::from("slot1"),
+                            item: String::from("Upslash"),
+                            receiver: String::from("slot2")
+                        }
+                    ],
+                    vec![SpoilerEntry {
+                        location: String::from("AF (Depths): Upper Right Chest (1st Reward)"),
+                        sender: String::from("slot1"),
+                        item: String::from("Gate: Outskirts to Drainage System"),
+                        receiver: String::from("slot2")
+                    }],
+                    vec![SpoilerEntry {
+                        location: String::from("Goal"),
+                        sender: String::from("slot1"),
+                        item: String::from("Victory"),
+                        receiver: String::from("slot1")
+                    }]
+                ]
+            )
         );
     }
 }
